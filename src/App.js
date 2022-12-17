4734026c77dfb49f50707af56633be357f6709f2
abdullayevazizbek <abdullayevaziz2006@gmail.com>
2022-12-17 15:13:46 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import Cart from "./components/Cart";
+import Navbar from "./components/Navbar";
+import ProdactList from "./components/ProdactList.";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [open, setOpen] = useState(false)
 
-export default App;
+    return (
+        <div className="App">
+            <Navbar cartOpen={setOpen} />
+            <ProdactList />
+            <Cart  open={open} setOpen={setOpen}/>
+        </div>
+    )
+}
+export default App
